78b97a2a63c7433d4e7c71a218f7480a5be3a133
crystal <793832732@qq.com>
2016-03-06 15:17:31 +0800

    add fixtures.js

---
+++ test/fixtures.js
@@ -0,0 +1,19 @@
+function loadNumberDirectory() {
+
+  var numberList = [
+    '', 'one', 'two', 'three', 'four', 'five',
+    'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+    'twelve','thirteen', 'fourteen', 'fifteen', 'sixteen',
+    'seventeen','eighteen', 'nineteen'];
+
+  var decadeList = [
+    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+    'eighty', 'ninety'];
+
+  return [numberList,decadeList];
+}
+
+function loadUnits() {
+  var units = ['', 'thousand', 'million', 'billion'];
+  return units;
+}

137accab0f53678ad0ecfd5e9adfa42cacc521b7
crystal <793832732@qq.com>
2016-03-06 16:29:23 +0800

    add transformNumberArray()

--- main/main.js
+++ main/main.js
@@ -13,3 +13,57 @@ function getNumberString(input) {
 
   return numberArray;
 }
+
+function transformNumberArray(numberArray) {
+  var numberString = [];
+
+  for(var i = 0; i < numberArray.length; i++) {
+    var stringElement;
+    if(parseInt(numberArray[i]) < 100) {
+      stringElement = transformDecades(numberArray[i]);
+    } else {
+      stringElement = transformHundreds(numberArray[i]);
+    }
+    numberString.push(stringElement);
+  }
+
+  return numberString;
+}
+
+function transformDecades(stringNumber) {
+
+  var numberDirectory = loadNumberDirectory();
+
+  var number = parseInt(stringNumber);
+  var result;
+  if(number < 20) {
+    result = numberDirectory[0][number];
+  } else {
+    var firstBit = parseInt(stringNumber.charAt(1));
+    var secondBit = parseInt(stringNumber.charAt(0));
+    if(firstBit === 0) {
+      result = numberDirectory[1][secondBit];
+    } else {
+      result = numberDirectory[1][secondBit] + ' ' + numberDirectory[0][firstBit]
+    }
+  }
+
+  return result;
+}
+
+function transformHundreds(stringNumber) {
+
+  var numberDirectory = loadNumberDirectory();
+  var thirdBit = parseInt(stringNumber.charAt(0));
+  var subStringNumber = stringNumber.substr(1,2) ;
+  var subStingForm = transformDecades(subStringNumber);
+
+  var result;
+  if(subStingForm === '') {
+    result = numberDirectory[0][thirdBit] + ' hundred';
+  } else {
+    result = numberDirectory[0][thirdBit] + ' hundred' + ' and ' + subStingForm;
+  }
+
+  return result;
+}
